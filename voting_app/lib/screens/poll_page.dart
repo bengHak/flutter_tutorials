a04099da11b78557daea98a3327bffab10d1ea3c
bengHak <41236155+bengHak@users.noreply.github.com>
2020-02-17 16:54:09 +0900

    투표항목 컴포넌트 생성

--- voting_app/lib/screens/poll_page.dart
+++ voting_app/lib/screens/poll_page.dart
@@ -59,6 +59,9 @@ class PollPage extends StatelessWidget {
                   child: Row(
                     children: <Widget>[
                       Icon(Icons.thumb_up),
+                      SizedBox(
+                        width: 5.0,
+                      ),
                       Text('27'),
                     ],
                   ),
@@ -77,8 +80,107 @@ class PollPage extends StatelessWidget {
                 ),
                 Text('5분전(작성시각)')
               ],
-            )
+            ),
+            Divider(),
+            Text('투표 항목'),
+            Container(
+              child: Column(
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      VotingItem(
+                        imagePath: 'images/flutter_logo.png',
+                        title: '플러터',
+                      ),
+                      VotingItem(
+                        imagePath: 'images/react_native_logo.png',
+                        title: '리액트 네이티브',
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+            Divider(),
+            Text('댓글'),
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class VotingItem extends StatelessWidget {
+  VotingItem({
+    this.imagePath,
+    this.title,
+  });
+
+  final String imagePath;
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Container(
+          width: double.infinity,
+          decoration: BoxDecoration(
+            color: Colors.lightBlue,
+            borderRadius: BorderRadius.circular(10.0),
+          ),
+          child: Column(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Image.asset(imagePath),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 5.0,
+              ),
+              Container(
+                child: Text(
+                  '$title',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 14.0,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(15.0),
+                  ),
+                  child: Center(
+                    child: Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: Text(
+                        '투표하기',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
